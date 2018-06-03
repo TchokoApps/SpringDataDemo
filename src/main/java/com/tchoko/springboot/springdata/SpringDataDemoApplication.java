348e7847f9c567b1e47e96221c496d5616dbf7f0
Tchoko Apps <tchokoapps@gmail.com>
2018-06-03 20:44:28 +0200

    c001

---
+++ src/main/java/com/tchoko/springboot/springdata/SpringDataDemoApplication.java
@@ -0,0 +1,12 @@
+package com.tchoko.springboot.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataDemoApplication.class, args);
+	}
+}
